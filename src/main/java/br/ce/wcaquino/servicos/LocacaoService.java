109a0647c8cea52cb601c5394f718a9f471c4c14
Yuster Almeida <yusteralmeida@gmail.com>
2021-02-25 20:27:17 -0300

    Uma troca de Date() para Calender()

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -51,7 +51,7 @@ public class LocacaoService {
 		Locacao locacao = new Locacao();
 		locacao.setFilmes(filmes);
 		locacao.setUsuario(usuario);
-		locacao.setDataLocacao(new Date());
+		locacao.setDataLocacao(Calendar.getInstance().getTime());
 		Double valorTotal = 0d;
 
 		for (int i = 0; i < filmes.size(); i++) {
@@ -78,7 +78,7 @@ public class LocacaoService {
 
 		locacao.setValor(valorTotal);
 		// Entrega no dia seguinte
-		Date dataEntrega = new Date();
+		Date dataEntrega = Calendar.getInstance().getTime();
 		dataEntrega = adicionarDias(dataEntrega, 1);
 
 		if (DataUtils.verificarDiaSemana(dataEntrega, Calendar.SUNDAY)) {
